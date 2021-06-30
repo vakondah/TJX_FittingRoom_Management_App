032473d65168e0412b47be6f82b0375b7b82c7e3
Aliaksandra Hrechka <vakondah@gmail.com>
2021-06-29 23:54:04 -0600

    Add project files.

---
+++ JavaScript.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $(".stall").click(function () {
+        let bgColor = $(this).css('background-color');
+        //alert(bgColor);
+        if (bgColor === 'rgb(255, 0, 0)') {
+            alert("OK");
+            $(this).css('background-color', 'blue');
+        }
+        else if (bgColor === 'rgb(0, 0, 255)'){
+            $(this).css('background-color', 'green');
+        }
+        else {
+            $(this).css('background-color', 'red');
+        }
+        
+        
+    });
+});
